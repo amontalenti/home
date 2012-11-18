64965cc35c97cdc8538fa18c8c79a2ec87ca628d
Andrew Montalenti <andrew@parsely.com>
2012-11-17 23:33:07 -0500

    add nginx index file

--- fabfile.py
+++ fabfile.py
@@ -22,6 +22,8 @@ def _render_etc_hosts(block=True):
     return contents
 
 def _overwrite_hosts_file(contents):
+    indexfile = path.join(DOT_HOME, "static", "index.html")
+    local("sudo cp {0} /usr/share/nginx/www/".format(indexfile))
     scratch_dir = path.join(DOT_HOME, "scratch")
     local("mkdir -p {0}".format(scratch_dir))
     filename = path.join(scratch_dir, "hosts")
