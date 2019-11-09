03e41c0f7a9edafe8be5f48f8399059c8f5964b5
Andrew Montalenti <andrew@parsely.com>
2019-11-09 16:24:29 -0500

    Add rebel-readline (lein rebl) to Clojure config

---
+++ .lein/profiles.clj
@@ -0,0 +1,14 @@
+{
+ :user
+  {
+    :plugins [
+      [lein-pprint "1.1.1"]
+    ],
+    :dependencies [
+      [com.bhauman/rebel-readline "0.1.4"]
+    ],
+    :aliases {
+      "rebl" ["trampoline" "run" "-m" "rebel-readline.main"]
+    }
+  }
+}
